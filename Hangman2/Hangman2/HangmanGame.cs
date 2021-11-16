269cb13849df1b5df5c22733c17a1ec8ec8bbb09
Rheitheillaethor <minegocu3@gmail.com>
2021-11-16 17:42:40 +0100

    1

--- Hangman2/Hangman2/HangmanGame.cs
+++ Hangman2/Hangman2/HangmanGame.cs
@@ -15,10 +15,10 @@ namespace Hangman2
         {
             //1. read file with words to guess
             //...
-
+            string category = RandomizeTheCategory();
             //2. Get random password
             //...
-            var randomWord = "hangman";
+            var randomWord = RandomizeThePhrase(category);
 
             gameConfig = new Game(randomWord);
 
@@ -34,6 +34,7 @@ namespace Hangman2
         {
             //4. Clear console screen
             //... Write user current score / word with guessed characters like 'han_g_an' / used characters
+            Console.Clear();
 
             //5. Ready user input
             //If it was already used then write that information on console
@@ -45,5 +46,39 @@ namespace Hangman2
             gameConfig.MarkCharacterAsUsed(userInput);
             gameConfig.AddPoint();
         }
+
+        static string RandomizeTheCategory()
+        {
+            Random rnd = new Random();
+            int randomCategory = rnd.Next(0, 4);
+            switch (randomCategory)
+            {
+                case 0:
+                    Console.WriteLine("Computer choose fruits");
+                    return "fruits.txt";
+                case 1:
+                    Console.WriteLine("Computer choose animals");
+                    return "animals.txt";
+                case 2:
+                    Console.WriteLine("Computer choose sport");
+                    return "sport.txt";
+                case 3:
+                    Console.WriteLine("Computer choose countries");
+                    return "countries.txt";
+                default:
+                    return RandomizeTheCategory();
+            }
+        }
+
+        static string RandomizeThePhrase(string TextFileName)
+        {
+            int lineCount = File.ReadAllLines(TextFileName).Length;
+            Random rnd = new Random();
+            int randomLine = rnd.Next(0, lineCount);
+            string wordToGuess = File.ReadLines(TextFileName).Skip(randomLine - 1).Take(1).First();
+            Console.WriteLine("Generated phrase is: " + wordToGuess);
+            return wordToGuess;
+        }
+
     }
 }
